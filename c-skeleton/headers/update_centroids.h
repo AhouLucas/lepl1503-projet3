1e1a5da4c16fa24963d1babbb88d0da8fe545948
Maxime <maxime.decaluwe@student.uclouvain.be>
2024-03-12 15:10:14 +0100

    1er essai

---
+++ c-skeleton/headers/update_centroids.h
@@ -0,0 +1,21 @@
+#ifndef UPDATE_CENTROIDS_H
+#define UPDATE_CENTROIDS_H
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+#include "point.h"
+#include "distance.h"
+
+/**
+ * @brief Compute the new centroids from the the current vectors
+ * 
+ * @param centroids Array of centroids of type point_t
+ * @param points Array of points of type point_t to be assigned to the closest centroid
+ * @return int -1 if an error occurred, 0 if no points have changed their closest centroid, 1 if at least one point has changed its closest centroid
+ */
+int update_centroids(point_t* centroids, point_t* points);
+
+#endif // UPDATE_CENTROIDS_H
---
+++ c-skeleton/src/update_centroids.c
@@ -0,0 +1,13 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#include "../headers/update_centroids.h"
+#include "../headers/point.h"
+#include "../headers/distance.h"
+
+int update_centroids(point_t* centroids, point_t* points){
+    int sum = 0;
+    int nbr_points = 0;
+    // TO DO
+    return 0;
+}
