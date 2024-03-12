e23eab16e1d4ac950ef3c2baf68f91a63c4e577e
Lucien Fiorini <lucien.fiorini@student.uclouvain.be>
2024-03-12 11:41:43 +0100

    add point definition

--- c-skeleton/headers/point.h
+++ c-skeleton/headers/point.h
@@ -1,9 +1,13 @@
-
 #ifndef POINT_H
 #define POINT_H
 
+#include <stddef.h>
+#include <stdint.h>
+
 typedef struct {
-    // TODO
+    int32_t dimension;
+    int64_t* coordinates;
+    uint32_t clusterID;
 } point_t;
 
 #endif //POINT_H
