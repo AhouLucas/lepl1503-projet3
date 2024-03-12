206cc8a34876dce8b0e6cbec3dcb32230d0dfdac
Maxime <maxime.decaluwe@student.uclouvain.be>
2024-03-12 16:28:51 +0100

    Début d'idée pour update_centroids

--- c-skeleton/headers/update_centroids.h
+++ c-skeleton/headers/update_centroids.h
@@ -7,15 +7,16 @@
 #include <stddef.h>
 
 #include "point.h"
-#include "distance.h"
 
 /**
  * @brief Compute the new centroids from the the current vectors
  * 
  * @param centroids Array of centroids of type point_t
  * @param points Array of points of type point_t to be assigned to the closest centroid
+ * @param k Number of centroids
+ * @param num_points Number of points
  * @return int -1 if an error occurred, 0 if no points have changed their closest centroid, 1 if at least one point has changed its closest centroid
  */
-int update_centroids(point_t* centroids, point_t* points);
+int update_centroids(point_t* centroids, point_t* points, int k, int num_points);
 
 #endif // UPDATE_CENTROIDS_H
--- c-skeleton/src/update_centroids.c
+++ c-skeleton/src/update_centroids.c
@@ -3,11 +3,32 @@
 
 #include "../headers/update_centroids.h"
 #include "../headers/point.h"
-#include "../headers/distance.h"
 
-int update_centroids(point_t* centroids, point_t* points){
-    int sum = 0;
-    int nbr_points = 0;
-    // TO DO
+int update_centroids(point_t* centroids, point_t* points, int k, int num_points){
+    if(points == NULL) {
+        return -1;
+    }
+    int32_t dim = points->dimension;
+
+    uint32_t* clusters = (uint32_t*)malloc(k*sizeof(uint32_t));
+    int64_t* sumPos = (int64_t*)malloc(k*sizeof(int64_t[dim]));
+    int* pByClust = (int*)malloc(k*sizeof(int));
+
+    //Faire une liste des clusters grâce aux centroids
+    for(int c = 0; c<k; c++){
+        clusters[c]=centroids[c]->clusterID;
+    }
+
+    for(int i = 0; i<num_points; i++){
+        for(int clusterIndex = 0; clusterIndex<k; clusterIndex++){
+            if(points[i]->clusterID==clusters[clusterIndex]){
+                for(int eachDim = 0; eachDim<dim; eachDim++;){
+                    sumPos[clusterIndex][eachDim]+=points[i]->coordinates[eachDim];
+                }
+                pByClust[clusterIndex]++;
+            }
+        }
+    }
+
     return 0;
 }
