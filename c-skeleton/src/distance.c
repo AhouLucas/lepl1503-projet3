beb7f8ece8e46eda91b8bea0b9d06a00f18cd174
Samuel Ahou <samuelahou@hotmail.com>
2024-03-14 18:20:29 +0100

    Changed int64_t to uint64_t in distance functions

--- c-skeleton/src/distance.c
+++ c-skeleton/src/distance.c
@@ -1,6 +1,6 @@
 #include "../headers/distance.h"
 
-int64_t squared_manhattan_distance(const point_t *p1, const point_t *p2) {
+uint64_t squared_manhattan_distance(const point_t *p1, const point_t *p2) {
     int64_t sum = 0;
     int64_t diff = 0;
     for (uint32_t i = 0; i < p1->dimension; i++) {
@@ -16,7 +16,7 @@ int64_t squared_manhattan_distance(const point_t *p1, const point_t *p2) {
 }
 
 
-int64_t squared_euclidean_distance(const point_t *p1, const point_t *p2) {
+uint64_t squared_euclidean_distance(const point_t *p1, const point_t *p2) {
     int64_t sum = 0;
     int64_t diff = 0;
     for (uint32_t i = 0; i < p1->dimension; i++) {
